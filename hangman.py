c3751392f7b2cee16183e05bc312592cddab771a
Kiana <salmasikiana@gmail.com>
2023-06-14 16:11:33 -0600

    Check if the user is wrong If the letter is not in the chosen_word, print out the letter and let them know it's not in the word

--- hangman.py
+++ hangman.py
@@ -1,4 +1,4 @@
-import random 
+import random
 from hangman_words import word_list
 
 chosen_word = random.choice(word_list)
@@ -8,9 +8,10 @@ end_of_game = False
 lives = 6
 
 from hangman_art import logo
+
 print(logo)
 
-print(f'Pssst, the solution is {chosen_word}.')
+print(f"Pssst, the solution is {chosen_word}.")
 
 display = []
 for _ in range(word_length):
@@ -26,3 +27,11 @@ for position in range(word_length):
     letter = chosen_word[position]
     if letter == guess:
         display[position] = letter
+
+
+if guess not in chosen_word:
+    print(f"You guessed {guess}, that's not in the word. You lose a life.")
+    lives -= 1
+    if lives == 0:
+        end_of_game = True
+        print("You lose.")
